a50b05d9454fc31830cfd9ac1749cccb7af9b0d6
Johan-RC <rodriguezcharryjohansteven@gmail.com>
2026-04-22 07:20:01 -0500

    Update

--- Api-Django/tasks/models.py
+++ Api-Django/tasks/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    autor = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
